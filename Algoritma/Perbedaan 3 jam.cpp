b5c482dc4e82b7ff8e14da7a16a39d6b6df5cca5
geraldyfatullah <geraldyfatullah@gmail.com>
2020-01-08 16:59:56 +0700

    Tugas Perbedaan 3 Jam

--- Algoritma/Perbedaan 3 jam.cpp
+++ Algoritma/Perbedaan 3 jam.cpp
@@ -7,17 +7,33 @@ int main(){
 };
 	data_jam jam1, jam2, jam3;
 	cout<< "Masukkan jam pertama"<<endl;
-	cout<< "Jam	: "; cin>> jam1.j;
-	cout<< "Menit	: "; cin>> jam1.m;
-	cout<< "Detik	: "; cin>> jam1.d;
+	cout<< "Jam	= "; cin>> jam1.j;
+	cout<< "Menit	= "; cin>> jam1.m;
+	cout<< "Detik	= "; cin>> jam1.d;
 	cout<<endl;
 	cout<< "Masukkan jam kedua"<<endl;
-	cout<< "Jam	: "; cin>> jam2.j;
-	cout<< "Menit	: "; cin>> jam2.m;
-	cout<< "Detik	: "; cin>> jam2.d;
-	jam3.j=((jam2.j*3600)-(jam1.j*3600))/3600;
-	jam3.m=((jam2.m*3600)-(jam1.m*3600))/3600;
-	jam3.d=jam2.d-jam1.d;
+	cout<< "Jam	= "; cin>> jam2.j;
+	cout<< "Menit	= "; cin>> jam2.m;
+	cout<< "Detik	= "; cin>> jam2.d;
 	cout<<endl;
-	cout<< "Selisih : "<< jam3.j<< " : "<< jam3.m<< " : "<< jam3.d<<endl;
+	cout<< "Selisih = ";
+	if ((jam2.d - jam1.d) < 0) {
+		jam3.d = (60 + jam2.d) - jam1.d;
+		}
+	else {
+		jam3.d = jam2.d - jam1.d;
+		}
+	if ((jam2.m- jam1.m) < 0) {
+		jam3.m= (60 + jam2.m) - jam1.m - 1;
+		jam3.j = jam2.j - jam1.j - 1;
+		}
+	else {
+		jam3.m = jam2.m- jam1.m;
+		jam3.j = jam2.j - jam1.j;
+		}
+	if ((jam2.d - jam1.d) < 0, (jam2.m- jam1.m) < 0, jam2.j < jam1.j){
+		jam3.j = jam2.j + 11 - jam1.j;
+	}
+	cout<<jam3.j<<":"<<jam3.m<<":"<<jam3.d<<endl;
+
 }
